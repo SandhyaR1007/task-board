f651d98bd4ecbb373fc4e771d1d6cfa1a70daf52
SandhyaR1007 <47480884+SandhyaR1007@users.noreply.github.com>
2023-08-16 23:16:54 +0530

    update: configure Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { TaskProvider } from "./context/TaskContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Router>
-    <App />
-  </Router>
+  <TaskProvider>
+    <Router>
+      <App />
+    </Router>
+  </TaskProvider>
 );
