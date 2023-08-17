4757a09d249fbad2382ea9c894fa3065d35c6431
SandhyaR1007 <47480884+SandhyaR1007@users.noreply.github.com>
2023-08-17 18:17:13 +0530

    update: added routing in menubar

--- src/components/layout/Menubar.jsx
+++ src/components/layout/Menubar.jsx
@@ -1,5 +1,33 @@
+import { AiFillFolder } from "react-icons/ai";
+import { BsBarChartSteps } from "react-icons/bs";
+import { NavLink } from "react-router-dom";
 const Menubar = () => {
-  return <div className="bg-gray-800 text-white  p-1">Menubar</div>;
+  return (
+    <div className="bg-gray-800 text-white  px-2 py-8 w-16">
+      <ul className="flex flex-col gap-5 items-center">
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "text-emerald-500" : "text-white"
+            }
+          >
+            <AiFillFolder className="text-2xl" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/analytics"
+            className={({ isActive }) =>
+              isActive ? "text-emerald-500" : "text-white"
+            }
+          >
+            <BsBarChartSteps className="text-2xl" />
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Menubar;
