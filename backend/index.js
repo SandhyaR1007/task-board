f247c09f6ab45e6b1b5dd3d17d372689c3178e69
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-08-29 21:54:08 +0530

    add: initialize backend for kanban board

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+
+const app = express();
+
+app.listen(8000);
+console.log("listening to port 8000");
---
+++ backend/models/taskModel.js
@@ -0,0 +1,16 @@
+const { mongoose } = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    unique: true,
+  },
+  summary: String,
+  assignee: String,
+  date: Date,
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
+s;
