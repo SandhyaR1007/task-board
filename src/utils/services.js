f2b8819c0b3530c3c9e587a33abd6cab090541fb
Sandhya Rajwanshi <47480884+SandhyaR1007@users.noreply.github.com>
2024-09-04 22:56:36 +0530

    update: add backend url

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,13 +1,6 @@
 import axios from "axios";
 
-const apiUrl = " https://gcp-mock.apiwiz.io/v1/tasks";
-const apiKey = "b4349714-47c7-4605-a81c-df509fc7e653";
-
 export const getTasksService = async () => {
-  const config = {
-    headers: {
-      "x-tenant": apiKey,
-    },
-  };
-  return axios.get(apiUrl, config);
+  console.log(import.meta.env.VITE_BASE_URL);
+  return axios.get(`${import.meta.env.VITE_BASE_URL}/task`, {});
 };
